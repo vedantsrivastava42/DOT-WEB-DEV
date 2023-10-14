eb2718f862b9d08d352efb65dc5718f75f738b4d
vedantsrivastava42 <vedantsrivastava42@gmail.com>
2023-10-14 12:24:19 +0530

    javascript codes added

--- JAVASCRIPT/fetchapi.js
+++ JAVASCRIPT/fetchapi.js
@@ -4,15 +4,39 @@
 //     console.log(output);
 // }
 // utility();  
-let options ={
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-};
-let content = fetch('https://jsonplaceholder.typicode.com/posts',options);
+
+// async function helper(){
+// let options ={
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// };
+// let content = await fetch('https://jsonplaceholder.typicode.com/posts',options);
+// let response = content.json();
+// return response;
+// }
+// async function utility(){
+//   let ans=helper();
+//   console.log(ans);
+// }
+// utility();
+
+ //CLOSURE
+
+ function outer(x){
+  function inner(y){
+      console.log(x+y+z);
+  }
+  let z = 100;
+
+  return inner;
+}
+
+let myFunc = outer(5);
+myFunc(10);
